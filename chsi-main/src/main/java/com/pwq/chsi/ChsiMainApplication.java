71e62b575ba5257c33d65625cd728971ad98a9ee
PwqGithub <15001859181@163.com>
2018-04-22 22:22:51 +0800

    项目搭建

---
+++ chsi-main/src/main/java/com/pwq/chsi/ChsiMainApplication.java
@@ -0,0 +1,12 @@
+package com.pwq.chsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChsiMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChsiMainApplication.class, args);
+	}
+}
